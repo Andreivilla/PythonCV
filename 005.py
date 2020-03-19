6e51816542a8bd360db9e9512bf492d8c42e37cb
Andrei Villa <andreivilla63@gmail.com>
2020-03-19 20:11:21 -0300

    002

---
+++ 005.py
@@ -0,0 +1,2 @@
+n = int(input('digite um  numero: '))
+print('Analizando o valor {}, o antecessor é {} e o sucessor é {}'. format(n,(n-1),(n+1)))
---
+++ 006.py
@@ -0,0 +1,4 @@
+n = int(input('digite um numero')
+print('dobro: {}'. format((2*n)))
+print('triplo: {}'. format((3*n)))
+print('raiz: {}'. format((n**1/2)))
